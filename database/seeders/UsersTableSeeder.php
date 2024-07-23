6bdb95689801ba36a49c3398fbeb93aa87bc0bfd
Lec <me.alec.escasa@gmail.com>
2024-07-23 08:48:59 +0800

    Added more users

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -23,20 +23,6 @@ class UsersTableSeeder extends Seeder
                 'password' => bcrypt('mentorSecrets'), // password
                 'remember_token' => null,
             ],
-            [
-                'name' => '1stAdmin',
-                'email'=>'1st_admin@email.com',
-                'email_verified_at' => null,
-                'password'=> bcrypt('1stSecrets'),
-                'remember_token' => null,
-            ],
-            [
-               'name' => '2ndAdmin',
-               'email'=>'2nd_admin@email.com',
-               'email_verified_at' => null,
-               'password'=> bcrypt('2ndSecrets'),
-               'remember_token' => null,
-            ],
         ];
 
         foreach ($users as $user) {
