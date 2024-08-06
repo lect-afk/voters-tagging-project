863269c086b10b43434c66f400f072a064e54673
Lec <me.alec.escasa@gmail.com>
2024-08-06 16:22:48 +0800

    Tagging Responsiveness

--- resources/views/admin/pages/tagging/leader_table_body.blade.php
+++ resources/views/admin/pages/tagging/leader_table_body.blade.php
@@ -1,16 +1,27 @@
 @foreach ($leaders  as $leader)
     <tr>
-        <td>{{ $leader->firstname }} {{ $leader->middlename }} {{ $leader->lastname }}</td>
-        <td>{{ $leader->barangays->name }}</td>
+        <td class="align-middle">{{ $leader->firstname }} {{ $leader->middlename }} {{ $leader->lastname }}</td>
+        <td class="align-middle">{{ $leader->barangays->name }}</td>
         {{-- <td>{{ $leader->puroks->name  }}</td>
         <td>{{ $leader->sitios->name }}</td> --}}
-        <td>
-            <a class="icon-link" href="{{ route('voter_profile.manageleader', $leader->id) }}" title="Manage Subordinate">
-                <i class="fa-solid fa-user-gear"></i>
-            </a>
-            <a class="icon-link" href="{{ route('voter_profile.viewhierarchy', $leader->id) }}" title="View Hierarchy">
-                <i class="fa-solid fa-users-viewfinder"></i>
-            </a>
-        </td>
+        <td class="align-middle">
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    Actions
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <li>
+                        <a class="dropdown-item" href="{{ route('voter_profile.manageleader', $leader->id) }}">
+                            <i class="fa-solid fa-user-gear"></i> Manage Subordinate
+                        </a>
+                    </li>
+                    <li>
+                        <a class="dropdown-item" href="{{ route('voter_profile.viewhierarchy', $leader->id) }}">
+                            <i class="fa-solid fa-users-viewfinder"></i> View Hierarchy
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </td>        
     </tr>
 @endforeach
--- resources/views/admin/pages/tagging/namelist.blade.php
+++ resources/views/admin/pages/tagging/namelist.blade.php
@@ -2,24 +2,34 @@
 
 @section('content')
     <div class="container my-5">
-        <div class="d-flex justify-content-between align-items-center mb-3">
-            <h1>List Of Leaders</h1>
-            {{ $leaders->appends(['query' => request('query'), 'leader' => request('leader')])->links('admin.pages.partials.pagination') }}
+        <div class="row mb-3">
+            <div class="col-12 col-md-6">
+                <h1>List Of Leaders</h1>
+            </div>
+            <div class="col-12 col-md-6 d-flex justify-content-md-end">
+                {{ $leaders->appends(['query' => request('query'), 'leader' => request('leader')])->links('admin.pages.partials.pagination') }}
+            </div>
         </div>
-        <form class="d-flex" method="GET" action="{{ route('leaders.search') }}" role="search">
-            <input id="searchInput" name="query" class="form-control me-2 w-50" type="search" placeholder="Search" aria-label="Search" value="{{ request('query') }}">
-            <select id="leaderFilter" name="leader" class="form-select w-50">
-                <option value="Barangay" {{ request('leader') == 'Barangay' ? 'selected' : '' }}>Barangay Leader</option>
-                <option value="Purok" {{ request('leader') == 'Purok' ? 'selected' : '' }}>Purok Leader</option>
-                <option value="Municipal" {{ request('leader') == 'Municipal' ? 'selected' : '' }}>Municipal Leader</option>
-                <option value="District" {{ request('leader') == 'District' ? 'selected' : '' }}>District Leader</option>
-                <option value="Provincial" {{ request('leader') == 'Provincial' ? 'selected' : '' }}>Provincial Leader</option>
-                <option value="Regional" {{ request('leader') == 'Regional' ? 'selected' : '' }}>Regional Leader</option>
-            </select>
-            <button class="ms-2 button-index" type="submit">
-                <i class="fa-solid fa-magnifying-glass fa-xl"></i>
-                <span class="fw-semibold ms-2">Search</span>
-            </button>
+        <form class="row g-2" method="GET" action="{{ route('leaders.search') }}" role="search">
+            <div class="col-12 col-md-6">
+                <input id="searchInput" name="query" class="form-control" type="search" placeholder="Search" aria-label="Search" value="{{ request('query') }}">
+            </div>
+            <div class="col-12 col-md-4">
+                <select id="leaderFilter" name="leader" class="form-select">
+                    <option value="Barangay" {{ request('leader') == 'Barangay' ? 'selected' : '' }}>Barangay Leader</option>
+                    <option value="Purok" {{ request('leader') == 'Purok' ? 'selected' : '' }}>Purok Leader</option>
+                    <option value="Municipal" {{ request('leader') == 'Municipal' ? 'selected' : '' }}>Municipal Leader</option>
+                    <option value="District" {{ request('leader') == 'District' ? 'selected' : '' }}>District Leader</option>
+                    <option value="Provincial" {{ request('leader') == 'Provincial' ? 'selected' : '' }}>Provincial Leader</option>
+                    <option value="Regional" {{ request('leader') == 'Regional' ? 'selected' : '' }}>Regional Leader</option>
+                </select>
+            </div>
+            <div class="col-12 col-md-2">
+                <button class="button-index w-100" type="submit">
+                    <i class="fa-solid fa-magnifying-glass fa-md"></i>
+                    <span class="fw-semibold ms-2">Search</span>
+                </button>
+            </div>
         </form>
         
         <table class="table mt-2 table-light table-hover">
@@ -27,7 +37,7 @@
                 <tr>
                     <th>Full Name</th>
                     <th>Barangay</th>
-                    <th style="width: 15%;">Actions</th>
+                    <th style="width: 15%;"></th>
                 </tr>
             </thead>
             <tbody class="leader-table-body">
